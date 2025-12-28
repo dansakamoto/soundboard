bcc7b84f35f5f17dd3cec17ec99b75ccd1f23f99
Dan Sakamoto <contact@dansakamoto.com>
2025-12-28 00:46:17 -0500

    feat: add number string validator

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,7 +2,7 @@ import { characters } from "./_data/characters";
 
 import type { KanjiGroup } from "./types";
 
-export default class AudioHandler {
+export class AudioHandler {
   constructor() {
     this.setupAudio();
   }
@@ -44,4 +44,65 @@ export default class AudioHandler {
       this.currentlyPlaying = "";
     }
   }
+}
+
+export function isValidGroup(s: string) {
+  return isValidNumber(s);
+}
+
+function isValidNumber(s: string) {
+  const validSteps = ["兆", "億", "万"];
+  let test = "";
+
+  if (s === "") return false;
+
+  for (const c of s) {
+    if (validSteps.includes(c)) {
+      if (test === "" || !isValidSmallChunk(test)) return false;
+
+      test = "";
+
+      while (validSteps[0] !== c) validSteps.shift();
+      validSteps.shift();
+    } else {
+      test += c;
+    }
+  }
+
+  if (test === "") return true;
+
+  return isValidSmallChunk(test);
+}
+
+function isValidSmallChunk(s: string) {
+  let singleValid = true;
+  const validDigits = ["二", "三", "四", "五", "六", "七", "八", "九"];
+  const validSteps = ["千", "百", "十"];
+
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+
+    if (c === "一") {
+      if (i === s.length - 1) return true;
+      return false;
+    }
+
+    if (validDigits.includes(c)) {
+      if (!singleValid) return false;
+
+      singleValid = false;
+      continue;
+    }
+
+    if (validSteps.includes(c)) {
+      singleValid = true;
+      while (validSteps[0] !== c) validSteps.shift();
+      validSteps.shift();
+      continue;
+    }
+
+    return false;
+  }
+
+  return true;
 }
