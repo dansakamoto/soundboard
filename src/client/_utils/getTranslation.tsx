8b7e1bb811dc5ae1dc88eb91ce158d7b673ca14c
Dan Sakamoto <contact@dansakamoto.com>
2025-12-28 18:40:28 -0500

    fix: consistently format numbers

--- src/client/_utils/getTranslation.tsx
+++ src/client/_utils/getTranslation.tsx
@@ -2,6 +2,8 @@ import { kanji2number } from "@geolonia/japanese-numeral";
 import { characters } from "../_data/characters";
 import { isValidNumber } from "./validators";
 
+const numFormatter = new Intl.NumberFormat("en-US");
+
 const translations: Record<string, string> = {};
 
 for (const c of characters) {
@@ -10,6 +12,6 @@ for (const c of characters) {
 
 export default function getTranslation(s: string) {
   if (s in translations) return translations[s];
-  if (isValidNumber(s)) return kanji2number(s);
+  if (isValidNumber(s)) return numFormatter.format(kanji2number(s));
   return "";
 }
